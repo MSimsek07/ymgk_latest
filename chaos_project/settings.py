4a5ddee896647a48a1765dba1f707d6eaa6aa4b5
MSimsek07 <masimsek.official@gmail.com>
2024-05-20 23:17:43 +0300

    fixed debugger

--- chaos_project/settings.py
+++ chaos_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_(%59!ihe2*2k(p&qxk+nzczr(bm%7_j6sfet)+v0*k2p)=z1l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ymgk-latest.onrender.com']
 
